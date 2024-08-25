4d407b4b24c896d71f15141e92c30bc9f871c4e2
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2024-08-25 16:43:42 +0530

    react-router-dom

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header-contents">
+            <h2>Order Your Daily Veggies from here.</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis rerum accusamus beatae mollitia doloribus sunt commodi minus magnam tenetur aspernatur aliquam quia soluta nobis, dolore est, hic dicta ipsa ipsam labore officia, unde culpa assumenda quae architecto. Reiciendis neque earum sequi atque aliquid eos vitae magnam, alias maiores voluptatem odit.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header

c14d566decb7561b84d7b28daf37b5f00824ef77
Utsav Singh <utsav3711@gmail.com>
2024-08-25 18:40:57 +0530

    Grid Complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
+import FoodDisply from '../../components/FoodDisplay/FoodDisply'
 
 const Home = () => {
   return (
     <div>
         <Header/>
+        <FoodDisply category ={category}  />
     </div>
   )
 }

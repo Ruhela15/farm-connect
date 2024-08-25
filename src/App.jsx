d847b455ff50b5794639a1549a47b9de3533c693
vansh <vanshsin784@gmail.com>
2024-08-25 17:14:45 +0530

    Explore update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import explore from './components/Explore Menu/exploreMenu'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Kya VOh maanegi?</h1>
+    <explore/>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,9 +3,11 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 
 const Home = () => {
+  const [category,setCategory]=useState("All");
   return (
     <div>
         <Header/>
+        <explore category={category} setCategory={setCategory}/>
     </div>
   )
 }

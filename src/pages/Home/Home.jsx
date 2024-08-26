4a2c149d9bc6e70f73354ae361b3ece79cfae6cc
Priyanshu <pr129858@gmail.com>
2024-08-26 16:30:57 +0530

    Login

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import './Home.css';
 import Header from '../../components/Header/Header';
-import Navbar from '../../components/navbar/Navbar';
-import Footer from '../../components/Footer/Footer';
 import AppDownload from '../../components/AppDownload/AppDownload';
 import FoodDisply from '../../components/FoodDisplay/FoodDisply';
 import ExploreMenu from '../../components/ExploreMenu/exploreMenu';
@@ -12,12 +10,10 @@ const Home = () => {
 
   return (
     <div>
-        <Navbar />
         <Header />
         <ExploreMenu category={category} setCategory={setCategory}/>
         <FoodDisply category={category} />
         <AppDownload />
-        <Footer />
     </div>
   );
 }

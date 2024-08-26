5132fb2f61e806d178dad71e09c9782291b00b48
vansh <vanshsin784@gmail.com>
2024-08-26 15:47:21 +0530

    Backend Setup

---
+++ Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './PlaceOrder.css'
+
+const PlaceOrder = () => {
+  return (
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className='title'>Delivery Information</p>
+        <div className="multi-field">
+          <input type="text" placeholder='First name' />
+          <input type="text" placeholder='Last name' />
+        </div>
+        <input type="email" placeholder='Email address' />
+        <input type="text" placeholder='Street' />
+        <div className="multi-field">
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State' />
+        </div>
+        <div className="multi-field">
+          <input type="text" placeholder='Zip Code' />
+          <input type="text" placeholder='Country' />
+        </div>
+        <input type="text" placeholder='Phone' />
+      </div>
+      <div className='place-order-right'>
+      <div className="cart-total">
+        <h2>Cart Totals</h2>
+        <div>
+          <div className="cart-total-details">
+            <p>Subtotal</p>
+            <p>${getTotalCartAmount()}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <p>Delivery Fee</p>
+            <p>${2}</p>
+          </div>
+        </div>
+      </div>
+      </div>
+    </form>
+  )
+}
+
+export default PlaceOrder

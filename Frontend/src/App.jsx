80dc682c92a71d03d2140a9dd1877879aae431b3
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2024-08-28 16:24:40 +0530

    Login Restored

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,23 +5,25 @@ import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
-import Navbar from './components/navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
+
 const App = () => {
-  const [showLogin,setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
-  <>
-    {showLogin?<LoginPopup/>:<></>}  
-    <div className="app">
-      <Navbar setShowLogin = {setShowLogin}/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element = {<Cart/>}/>
-        <Route path = '/order' element = {<PlaceOrder/>}/>
-      </Routes>
-      <Footer/>
-    </div>
+    <>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : null}
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+        </Routes>
+        <Footer />
+      </div>
     </>
   )
 }
 
-export default App
+export default App;

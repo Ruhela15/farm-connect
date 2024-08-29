92d6edad0b6ab7f9a7a918e6bdf4f42ce88641bf
vansh <vanshsin784@gmail.com>
2024-08-29 17:04:17 +0530

    Adding Navbar and Sidebar

---
+++ Admin/src/components/SideBar/Sidebar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Sidebar.css'
+import { assets } from '../../../admin_assets/assets'
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Add Item</p>
+
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>List Items</p>
+
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>Orders</p>
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Sidebar

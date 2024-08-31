e918f9ce7dc57083903bffc554d0a2d955475728
Utsav Singh <utsav3711@gmail.com>
2024-08-31 17:00:16 +0530

    Temporarily rename Navbar.jsx to TempNavbar.jsx to resolve case-sensitivity issue

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
 import Add from './pages/Add/Add';
 import List from './pages/List/List'
@@ -7,6 +6,7 @@ import Orders from './pages/Orders/Orders';
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/NavBar/Navbar';
 
 const App = () => {
 
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <>
     <ToastContainer/>
-      <Navbar />
+      <Navbar/>
       <hr />
       <div className="app-content">
         <Sidebar />

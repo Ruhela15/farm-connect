0cc1d6f0a38816ae3e0cf779bf357fa06138a457
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2024-08-31 17:20:11 +0530

    Your commit message

--- Frontend/src/components/context/Storecontext.jsx
+++ Frontend/src/components/context/Storecontext.jsx
@@ -5,7 +5,14 @@ export const Storecontext = createContext(null);
 
 const Storecontextprovider = (props) => {
   const [cartItems, setCartItems] = useState({});
+<<<<<<< HEAD:Frontend/src/components/context/Storecontext.jsx
   const [food_list,setFoodList] = useState([])
+=======
+  
+  const url = "http://localhost:4000"
+  const [token , settoken] = useState("")
+
+>>>>>>> e918f9ce7dc57083903bffc554d0a2d955475728:Frontend/src/components/content/Storecontext.jsx
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
@@ -29,6 +36,7 @@ const Storecontextprovider = (props) => {
     return totalAmount;
   }
 
+<<<<<<< HEAD:Frontend/src/components/context/Storecontext.jsx
   const fetchFoodList = async() => {
     const response = await axios.get(url + "/api/food/list");
     setFoodList(response.data.data)
@@ -43,6 +51,13 @@ const Storecontextprovider = (props) => {
     }
     loadData();
   })
+=======
+  useEffect(()=>{
+    if(localStorage.getItem("token")){
+      settoken(localStorage.getItem("token"));
+    }
+  },[])
+>>>>>>> e918f9ce7dc57083903bffc554d0a2d955475728:Frontend/src/components/content/Storecontext.jsx
 
   const contextvalue = {
     food_list,
@@ -51,6 +66,9 @@ const Storecontextprovider = (props) => {
     addToCart,
     removeFromCart,
     getTotalAmount,
+    url,
+    token,
+    settoken
   };
   return (
     <Storecontext.Provider value={contextvalue}>
